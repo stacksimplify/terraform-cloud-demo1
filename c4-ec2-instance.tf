dc4184ae6ef45e8049c3db63642906d345c499bf
Kalyan Reddy Daida <stacksimplify@gmail.com>
2021-03-06 16:40:32 +0530

    Changed EC2 Instances from 2 to 3

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
-  count = 2
+  count = 3
   key_name      = "terraform-key"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
